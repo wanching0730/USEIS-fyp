dd3292d9c7cb837253e9971d44655f530f5ea3de
wanching_97 <wanching_97@hotmail.com>
2018-05-30 10:23:48 +0800

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Router, browserHistory, Route, Link } from 'react-router';
 import './App.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <Link to="/">Feed</Link>
+    <Link to="/profile">Profile</Link>
+  </div>
+);
+
+// props is 'title'
+const Template = ( { title }) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      This is the {title} page.
+    </p>
+  </div>
+);
+
+const Feed = (props) => (
+  <Template title="Feed" />
+);
+
+const Profile = (props) => (
+  <Template title = "Profile"/>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={Feed}/>
+        <Route path="/profile" component={Profile}/>
+      </Router>
     );
   }
 }
