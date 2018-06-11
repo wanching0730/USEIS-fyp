8909df163bacd09f90b6ee2551320b00aaf9ece2
wanching_97 <wanching_97@hotmail.com>
2018-06-12 01:56:07 +0800

    used axios for async

--- src/components/Society.js
+++ src/components/Society.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import $ from 'jquery';
+import axios from 'axios';
 
 class Society extends Component {
 
@@ -14,9 +16,12 @@ class Society extends Component {
     }
 
     listSocieties() {
-        // fetch(`http://localhost:5000/puppies`).then(result => result.json()).then(reply => console.log("result: " + reply));
         // fetch(`http://localhost:5000/puppies`).then(result => result.json()).then(reply => reply.map(row => console.log("row: " + row)));
         fetch(`http://localhost:5000/puppies`).then(result => result.json()).then(reply => this.setState({society: reply}));
+        // $.ajax({ type: "GET", url:`http://localhost:5000/puppies`, success: function(res) {
+        //     this.setState({society:res});
+        //  }});
+        //axios.get("http://localhost:5000/puppies").then(response => this.setState({society: response.data}));
     }
 
     render() {
