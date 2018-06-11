f2887936342b7687dfc97093ae2bf5f2a5c23815
wanching_97 <wanching_97@hotmail.com>
2018-06-12 00:45:08 +0800

    connected frontend to backend

--- src/components/Society.js
+++ src/components/Society.js
@@ -3,11 +3,25 @@ import NavBar from './NavBar';
 
 class Society extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {society: ""};
+    }
+
+    componentDidMount() {
+        this.listSocieties();
+    }
+
+    listSocieties() {
+        fetch(`http://localhost:5000/puppies`).then(result => result.json()).then(reply => console.log("result: " + reply));
+    }
+
     render() {
         return (
             <div> 
                 <NavBar />
-                <div>Hello society</div>
+                <p>{this.state.society}</p>
             </div>
         );
     };
