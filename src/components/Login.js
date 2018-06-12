d6d3148666af0e14e48b1b5db5e34d29b271bee0
wanching_97 <wanching_97@hotmail.com>
2018-06-13 00:04:27 +0800

    added login and register form:

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,44 @@
-import React from 'react';
 import NavBar from './NavBar';
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
-const Login = () => {
-
+class Login extends Component {
+constructor(props){
+  super(props);
+  this.state={
+  username:'',
+  password:''
+  }
+ }
+render() {
     return (
-        <div>
-            <NavBar />
-            <div>Hello login</div>
-        </div>
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <NavBar />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
     );
+  }
+}
+const style = {
+ margin: 15,
 };
-
 export default Login;
