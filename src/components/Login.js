324bbdecebfbfa9a2509747d2d403a16c9971f47
wanching_97 <wanching_97@hotmail.com>
2018-06-13 00:12:10 +0800

    cleaned code

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,41 +5,44 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
 class Login extends Component {
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:''
-  }
- }
-render() {
 
+  constructor(props){
+    super(props);
+    this.state={
+    username:'',
+    password:''
+    }
+   }
+
+  render() {
+    
     const { RaisedButtonStyle } = styles;
 
     return (
       <div>
         <MuiThemeProvider>
           <div>
-          <NavBar />
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={RaisedButtonStyle} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
+            <NavBar />
+            <TextField
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange = {(event,newValue) => this.setState({username:newValue})}
+              />
+            <br/>
+              <TextField
+                type="password"
+                hintText="Enter your Password"
+                floatingLabelText="Password"
+                onChange = {(event,newValue) => this.setState({password:newValue})}
+                />
+              <br/>
+              <RaisedButton label="Submit" primary={true} style={RaisedButtonStyle} onClick={(event) => this.handleClick(event)}/>
+          </div>
+          </MuiThemeProvider>
       </div>
     );
   }
+
 }
 
 const styles = {
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,9 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import axios from 'axios';
+
 class Register extends Component {
+
   constructor(props){
     super(props);
     this.state={
@@ -14,51 +16,54 @@ class Register extends Component {
       password:''
     }
   }
+  
   render() {
+
     const { RaisedButtonStyle } = styles;
-    
+
     return (
       <div>
         <MuiThemeProvider>
           <div>
-          <NavBar />
-           <TextField
-             hintText="Enter your First Name"
-             floatingLabelText="First Name"
-             onChange = {(event,newValue) => this.setState({first_name:newValue})}
-             />
-           <br/>
-           <TextField
-             hintText="Enter your Last Name"
-             floatingLabelText="Last Name"
-             onChange = {(event,newValue) => this.setState({last_name:newValue})}
-             />
-           <br/>
-           <TextField
-             hintText="Enter your Email"
-             type="email"
-             floatingLabelText="Email"
-             onChange = {(event,newValue) => this.setState({email:newValue})}
-             />
-           <br/>
-           <TextField
-             type = "password"
-             hintText="Enter your Password"
-             floatingLabelText="Password"
-             onChange = {(event,newValue) => this.setState({password:newValue})}
-             />
-           <br/>
-           <RaisedButton label="Submit" primary={true} style={RaisedButtonStyle} onClick={(event) => this.handleClick(event)}/>
-          </div>
-         </MuiThemeProvider>
+            <NavBar />
+            <TextField
+              hintText="Enter your First Name"
+              floatingLabelText="First Name"
+              onChange = {(event,newValue) => this.setState({first_name:newValue})}
+              />
+            <br/>
+            <TextField
+              hintText="Enter your Last Name"
+              floatingLabelText="Last Name"
+              onChange = {(event,newValue) => this.setState({last_name:newValue})}
+              />
+            <br/>
+            <TextField
+              hintText="Enter your Email"
+              type="email"
+              floatingLabelText="Email"
+              onChange = {(event,newValue) => this.setState({email:newValue})}
+              />
+            <br/>
+            <TextField
+              type = "password"
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange = {(event,newValue) => this.setState({password:newValue})}
+              />
+            <br/>
+            <RaisedButton label="Submit" primary={true} style={RaisedButtonStyle} onClick={(event) => this.handleClick(event)}/>
+            </div>
+          </MuiThemeProvider>
       </div>
     );
   }
 }
+
 const styles = {
   RaisedButtonStyle: {
     margin: 15
   }
-  
 };
+
 export default Register;
