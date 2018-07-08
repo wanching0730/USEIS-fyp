ea266af682e859b740047dd912b415f7cf4de576
wanching_97 <wanching_97@hotmail.com>
2018-07-09 02:15:54 +0800

    edited MyProfile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import '../style/society.css';
 import { Link } from 'react-router';
+import { red400 } from 'material-ui/styles/colors';
 
 class MyProfile extends Component {
 
@@ -20,6 +21,8 @@ class MyProfile extends Component {
     }
 
     render() {
+
+        const { imageStyle } = styles;
         
         return (
             <div id="outerDiv"> 
@@ -28,24 +31,87 @@ class MyProfile extends Component {
                     <div className="row">
                         <div className="panel-body">
                             <table className="table table-hover table-dark" border="1">
-                                <thead>
+                            <thead>
                                     <tr>
-                                        <th>Societies</th>
-                                        <th>Events</th>
-                                        <th>Status</th>                   
+                                        <th>No.</th>
+                                        <th>Logo</th>
+                                        <th>Society</th>  
+                                        <th>Events</th>  
+                                        <th>Feedback Status</th>             
                                     </tr>
                                 </thead>
 
                                 <tbody>
-                                    {this.state.society.map(row => {
+                                    <tr>
+                                        <td>1</td>
+                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
+                                        <td><Link to={`/perSociety/1`}>IT Society</Link></td>
+                                        <td> 
+                                            <li><Link to={`/perEvent/1`}>WorkShop</Link></li>
+                                            <li><Link to={`/perEvent/1`}>KLESF</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Easy Parcel Talk</Link></li>
+                                        </td>
+                                        <td>
+                                            <li>Done</li>
+                                            <li><Link to={`/feedback`}>Undone</Link></li>
+                                            <li><Link to={`/feedback`}>Undone</Link></li>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
+                                        <td><Link to={`/perSociety/1`}>First Aid Society</Link></td>
+                                        <td> 
+                                            <li><Link to={`/perEvent/1`}>Cardio Night Run</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Blood Donation</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Adventure Camp</Link></li>
+                                        </td>
+                                        <td>
+                                            <li>Done</li>
+                                            <li><Link to={`/feedback`}>Undone</Link></li>
+                                            <li>Done</li>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
+                                        <td><Link to={`/perSociety/1`}>Engineering Society</Link></td>
+                                        <td> 
+                                            <li><Link to={`/perEvent/1`}>ES Camp</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Engineering Fiesta</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Annual General Meeting</Link></li>
+                                        </td>
+                                        <td>
+                                            <li>Done</li>
+                                            <li>Done</li>
+                                            <li>Done</li>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
+                                        <td><Link to={`/perSociety/1`}>Sport Society</Link></td>
+                                        <td> 
+                                            <li><Link to={`/perEvent/1`}>Night Run</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Colour Run</Link></li>
+                                            <li><Link to={`/perEvent/1`}>Sport Carnival</Link></li>
+                                        </td>
+                                        <td>
+                                            <li><Link to={`/feedback`}>Undone</Link></li>
+                                            <li><Link to={`/feedback`}>Undone</Link></li>
+                                            <li><Link to={`/feedback`}>Undone</Link></li>
+                                        </td>
+                                    </tr>
+                                   
+                                    {/* {this.state.society.map(row => {
                                         return (
-                                            <tr>     
-                                                <td><Link to={`/perSociety/`+row[0]}>{row[1]}</Link></td>                                           
+                                            <tr>
+                                                <td><Link to={`/perSociety/`+row[0]}>{row[0]}</Link></td>
                                                 <td>{row[1]}</td>
-                                                <td>{row[2]}</td>   
+                                                <td>{row[3]}</td>
                                             </tr>
                                         );
-                                    })}
+                                    })} */}
                                 </tbody>
                             </table>
                         </div>
@@ -56,5 +122,12 @@ class MyProfile extends Component {
     };
     
 };
+
+const styles = {
+    imageStyle: {
+        height: "50px",
+        width: "50px"
+    }
+}
 
 export default MyProfile;
