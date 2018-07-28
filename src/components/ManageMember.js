0e80910b7a0337cff9c4e1e9aba15efde050b8c5
wanching_97 <wanching_97@hotmail.com>
2018-07-28 20:54:15 +0800

    added approve member action

--- src/components/ManageMember.js
+++ src/components/ManageMember.js
@@ -4,6 +4,8 @@ import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import $ from 'jquery';
 import axios from 'axios';
 import '../style/society.css';
+import { confirmAlert } from 'react-confirm-alert'; 
+import * as FontAwesome from '../../node_modules/react-icons/lib/fa';
 import { Link } from 'react-router';
 
 class ManageMember extends Component {
@@ -20,6 +22,25 @@ class ManageMember extends Component {
 
     listSocieties() {
         fetch(`http://localhost:5000/puppies`).then(result => result.json()).then(reply => this.setState({society: reply}));
+    }
+
+    handleApprove() {
+        confirmAlert({
+            title: 'Approval Confirmation',
+            message: 'Are you sure to approve this member?',
+            buttons: [
+              {
+                label: 'Yes',
+                onClick: () => {
+                    console.log('Click Yes');
+                }
+              },
+              {
+                label: 'No',
+                onClick: () => console.log('Click No')
+              }
+            ]
+          })
     }
 
     render() {
@@ -49,7 +70,8 @@ class ManageMember extends Component {
                                         <th>Course</th>  
                                         <th>Year and Sem</th> 
                                         <th>Phone Number</th>   
-                                        <th>Email Address</th>                    
+                                        <th>Email Address</th>   
+                                        <th>Action</th>                 
                                     </tr>
                                 </thead>
 
@@ -62,6 +84,7 @@ class ManageMember extends Component {
                                         <td>Y1S3</td>
                                         <td>018-9900990</td>
                                         <td>henghao@hotmail.com</td>
+                                        <td><Link onClick={this.handleApprove}><FontAwesome.FaPlus /></Link></td>
                                     </tr>
                                     <tr> 
                                         <td>2</td>
@@ -71,6 +94,7 @@ class ManageMember extends Component {
                                         <td>Y1S3</td>
                                         <td>018-9900990</td>
                                         <td>chimeng@hotmail.com</td>
+                                        <td><Link onClick={this.handleApprove}><FontAwesome.FaPlus /></Link></td>
                                     </tr>
                                     <tr> 
                                         <td>3</td>
@@ -80,6 +104,7 @@ class ManageMember extends Component {
                                         <td>Y1S3</td>
                                         <td>018-8989898</td>
                                         <td>kenghuat@hotmail.com</td>
+                                        <td><FontAwesome.FaCheck /></td>
                                     </tr>
                                     <tr> 
                                         <td>4</td>
@@ -89,6 +114,7 @@ class ManageMember extends Component {
                                         <td>Y3S3</td>
                                         <td>012-2930560</td>
                                         <td>kenneth@hotmail.com</td>
+                                        <td><FontAwesome.FaCheck /></td>
                                     </tr>
                                     {/* {this.state.society.map(row => {
                                         return (
