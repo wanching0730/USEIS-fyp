62d9c4af57738e655d0eee00764f526df7ff76ee
wanching_97 <wanching_97@hotmail.com>
2018-07-30 01:04:05 +0800

    added back buttons for some pages

--- src/components/ManageCrew.js
+++ src/components/ManageCrew.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import $ from 'jquery';
-import axios from 'axios';
-import '../style/society.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { confirmAlert } from 'react-confirm-alert'; 
 import * as FontAwesome from '../../node_modules/react-icons/lib/fa';
 import { Link } from 'react-router';
+import '../style/society.css';
 
 class ManageCrew extends Component {
 
@@ -63,6 +63,8 @@ class ManageCrew extends Component {
     }
 
     render() {
+
+        const { RaisedButtonStyle } = styles;
         
         return (
             <div id="outerDiv"> 
@@ -77,91 +79,107 @@ class ManageCrew extends Component {
                     </Breadcrumb>
                 </div>
 
-                <div className="container" id="tableContainer">
-                    <div className="row">
-                        <div className="panel-body">
-                            <table className="table table-hover table-light" border="1">
-                                <thead>
-                                    <tr>
-                                        <th>No.</th>
-                                        <th>Name</th>
-                                        <th>IC Number</th>   
-                                        <th>Course</th>  
-                                        <th>Year and Sem</th> 
-                                        <th>Phone Number</th>   
-                                        <th>Email Address</th>   
-                                        <th>Position</th>  
-                                        <th colSpan="2">Actions</th>               
-                                    </tr>
-                                </thead>
+                <div>
+                    <MuiThemeProvider>
 
-                                <tbody>
-                                    <tr> 
-                                        <td>1</td>
-                                        <td>Lim Heng Hao</td>
-                                        <td>999999-99-9999</td>
-                                        <td>Software Engineering</td>
-                                        <td>Y1S3</td>
-                                        <td>018-9900990</td>
-                                        <td>henghao@hotmail.com</td>
-                                        <td>Secretary</td>
-                                        <td><Link onClick={this.handleApprove}><FontAwesome.FaPlus /></Link></td>
-                                        <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
-                                    </tr>
-                                    <tr> 
-                                        <td>2</td>
-                                        <td>Toh Chi Meng</td>
-                                        <td>777777-77-7777</td>
-                                        <td>Mechanical Engineering</td>
-                                        <td>Y1S3</td>
-                                        <td>018-9900990</td>
-                                        <td>chimeng@hotmail.com</td>
-                                        <td>Logistics HOD</td>
-                                        <td><Link onClick={this.handleApprove}><FontAwesome.FaPlus /></Link></td>
-                                        <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
-                                    </tr>
-                                    <tr> 
-                                        <td>3</td>
-                                        <td>Lim Keng Huat</td>
-                                        <td>888888-88-8888</td>
-                                        <td>Civil Engineering</td>
-                                        <td>Y1S3</td>
-                                        <td>018-8989898</td>
-                                        <td>kenghuat@hotmail.com</td>
-                                        <td>Vice Chairperson</td>
-                                        <td><FontAwesome.FaCheck /></td>
-                                        <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
-                                    </tr>
-                                    <tr> 
-                                        <td>4</td>
-                                        <td>Kenneth Teng</td>
-                                        <td>333333-33-3333</td>
-                                        <td>Electrical Engineering</td>
-                                        <td>Y3S3</td>
-                                        <td>012-2930560</td>
-                                        <td>kenneth@hotmail.com</td>
-                                        <td>Editorial Assistant</td>
-                                        <td><FontAwesome.FaCheck /></td>
-                                        <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
-                                    </tr>
-                                    {/* {this.state.society.map(row => {
-                                        return (
-                                            <tr>
-                                                <td><Link to={`/perSociety/`+row[0]}>{row[0]}</Link></td>
-                                                <td>{row[1]}</td>
-                                                <td>{row[3]}</td>
-                                            </tr>
-                                        );
-                                    })} */}
-                                </tbody>
-                            </table>
+                    <div className="container" id="tableContainer">
+                        <div className="row">
+                            <div className="panel-body">
+                                <table className="table table-hover table-light" border="1">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Name</th>
+                                            <th>IC Number</th>   
+                                            <th>Course</th>  
+                                            <th>Year and Sem</th> 
+                                            <th>Phone Number</th>   
+                                            <th>Email Address</th>   
+                                            <th>Position</th>  
+                                            <th colSpan="2">Actions</th>               
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr> 
+                                            <td>1</td>
+                                            <td>Lim Heng Hao</td>
+                                            <td>999999-99-9999</td>
+                                            <td>Software Engineering</td>
+                                            <td>Y1S3</td>
+                                            <td>018-9900990</td>
+                                            <td>henghao@hotmail.com</td>
+                                            <td>Secretary</td>
+                                            <td><Link onClick={this.handleApprove}><FontAwesome.FaPlus /></Link></td>
+                                            <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
+                                        </tr>
+                                        <tr> 
+                                            <td>2</td>
+                                            <td>Toh Chi Meng</td>
+                                            <td>777777-77-7777</td>
+                                            <td>Mechanical Engineering</td>
+                                            <td>Y1S3</td>
+                                            <td>018-9900990</td>
+                                            <td>chimeng@hotmail.com</td>
+                                            <td>Logistics HOD</td>
+                                            <td><Link onClick={this.handleApprove}><FontAwesome.FaPlus /></Link></td>
+                                            <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
+                                        </tr>
+                                        <tr> 
+                                            <td>3</td>
+                                            <td>Lim Keng Huat</td>
+                                            <td>888888-88-8888</td>
+                                            <td>Civil Engineering</td>
+                                            <td>Y1S3</td>
+                                            <td>018-8989898</td>
+                                            <td>kenghuat@hotmail.com</td>
+                                            <td>Vice Chairperson</td>
+                                            <td><FontAwesome.FaCheck /></td>
+                                            <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
+                                        </tr>
+                                        <tr> 
+                                            <td>4</td>
+                                            <td>Kenneth Teng</td>
+                                            <td>333333-33-3333</td>
+                                            <td>Electrical Engineering</td>
+                                            <td>Y3S3</td>
+                                            <td>012-2930560</td>
+                                            <td>kenneth@hotmail.com</td>
+                                            <td>Editorial Assistant</td>
+                                            <td><FontAwesome.FaCheck /></td>
+                                            <td><Link onClick={this.handleDelete}><FontAwesome.FaTrash /></Link></td>
+                                        </tr>
+                                        {/* {this.state.society.map(row => {
+                                            return (
+                                                <tr>
+                                                    <td><Link to={`/perSociety/`+row[0]}>{row[0]}</Link></td>
+                                                    <td>{row[1]}</td>
+                                                    <td>{row[3]}</td>
+                                                </tr>
+                                            );
+                                        })} */}
+                                    </tbody>
+                                </table>
+
+                                <div style= {{ textAlign: "center" }}>
+                                    <RaisedButton label="Back" primary={true} style={RaisedButtonStyle} onClick={(event) => window.history.back()}/>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                </div>
+                </MuiThemeProvider>
             </div>
+        </div>
         );
     };
     
 };
+
+const styles = {
+    RaisedButtonStyle: {
+        margin: 15
+    }
+}
 
 export default ManageCrew;
