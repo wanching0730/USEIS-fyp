6f175da0b9213469aba362ecac2495317d8060c9
wanching_97 <wanching_97@hotmail.com>
2018-08-01 16:31:21 +0800

    added buttons to booth registration:

--- src/components/RegisterBooth.js
+++ src/components/RegisterBooth.js
@@ -2,17 +2,32 @@ import React, {Component} from 'react';
 import Seatmap from './Seatmap.jsx';
 import NavBar from './NavBar';
 import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import '../style/main.scss';
 import '../style/main.css';
+import '../style/form.css';
 
 class RegisterBooth extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   componentDidMount() {
     window.scrollTo(0, 0);
   }
 
+  handleClick() {
+    
+  }
+
   render() {
+
+    const { RaisedButtonStyle } = styles;
+
     const rows = [
       [{ number: 1 }, {number: 2}, {number: '3', isReserved: true}, null, {number: '4'}, {number: 5}, {number: 6}],
       [{ number: 1, isReserved: true }, {number: 2, isReserved: true}, {number: '3', isReserved: true}, null, {number: '4'}, {number: 5}, {number: 6}],
@@ -22,24 +37,36 @@ class RegisterBooth extends Component {
   ];
   
   return (
+
     <div>
-      <NavBar />
+    <MuiThemeProvider>
 
-      <div style={{ margin: 20 }}>
-          <Breadcrumb>
-              <BreadcrumbItem><Link to={`/`}>Home</Link></BreadcrumbItem>
-              <BreadcrumbItem><Link to={`/society`}>Societies</Link></BreadcrumbItem>
-              <BreadcrumbItem><Link to={`/perSociety/1`}>Cardio Night Run</Link></BreadcrumbItem>
-              <BreadcrumbItem active>Register Booth</BreadcrumbItem>
-          </Breadcrumb>
+      <div>
+        <NavBar />
+
+        <div style={{ margin: 20 }}>
+            <Breadcrumb>
+                <BreadcrumbItem><Link to={`/`}>Home</Link></BreadcrumbItem>
+                <BreadcrumbItem><Link to={`/society`}>Societies</Link></BreadcrumbItem>
+                <BreadcrumbItem><Link to={`/perSociety/1`}>Cardio Night Run</Link></BreadcrumbItem>
+                <BreadcrumbItem active>Register Booth</BreadcrumbItem>
+            </Breadcrumb>
+        </div>
+
+        <h2>Booth Registration</h2>
+
+        <div className="container">
+          <Seatmap rows={rows} maxReservableSeats={3} alpha />
+        </div>
+
+        <div class="button-section">
+          <RaisedButton label="Submit" id="button2" primary={true} style={RaisedButtonStyle} onClick={(event) => this.handleClick(event)}/>
+          <RaisedButton label="Back" primary={true} style={RaisedButtonStyle} onClick={(event) => window.history.back()}/>
+        </div>
+
       </div>
-
-      <h2>Booth Registration</h2>
-
-      <div className="container">
-        <Seatmap rows={rows} maxReservableSeats={3} alpha />
-      </div>
-    </div>
+    </MuiThemeProvider>
+  </div>
   );
   
   // if (module.hot) {
@@ -53,6 +80,14 @@ class RegisterBooth extends Component {
 
   }
 
+}
+
+const styles = {
+  RaisedButtonStyle: {
+    margin: 15,
+    // display: "flex",
+    // justifyContent: "center"
+  }
 }
 
 export default RegisterBooth;
