5a125b71e3690c515ec2b83abe64a5570ec70b6a
wanching_97 <wanching_97@hotmail.com>
2018-08-07 02:18:38 +0800

    edited navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav,  NavItem, UncontrolledDropdown, 
-    DropdownToggle, DropdownMenu, DropdownItem, NavLink } from 'reactstrap';
+    DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Link } from 'react-router';
 import '../style/navbar.css';
 
@@ -37,61 +37,61 @@ class NavBar extends Component {
     }
 
     render() {
-    return (
-        <div>
-        <Navbar className="topnav" dark expand="md">
-            <NavbarBrand href="/"><img src={ require('../assets/images/utar.jpg') } /></NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-                <NavItem>
-                <Link to="/">Home</Link>
-                </NavItem>
-                <NavItem>
-                <Link to="/society">Societies</Link>
-                </NavItem>
-                <NavItem>
-                <Link to="/event">Events</Link>
-                </NavItem>
-                <NavItem>
-                <Link to="/recruitmentBooth">Booths</Link>
-                </NavItem>
-                <NavItem>
-                <Link to="/myProfile">My Profile</Link>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                    Manage
-                </DropdownToggle>
-                <DropdownMenu right>
-                    <DropdownItem name="createProfile" onClick={this.onClick}>
-                        Create Society Profile
-                    </DropdownItem>
-                    <DropdownItem name="submitProposal" onClick={this.onClick}>
-                        Submit Proposal
-                    </DropdownItem>
-                    <DropdownItem name="manageBooth" onClick={this.onClick}>
-                        Manage Booth
-                    </DropdownItem>
-                    <DropdownItem name="manageProposal" onClick={this.onClick}>
-                        Manage Proposal
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem name="faq" onClick={this.onClick}>
-                        FAQ
-                    </DropdownItem>
-                </DropdownMenu>
-                </UncontrolledDropdown>
-            </Nav>
-            <Nav className="ml-auto" navbar>
-            <NavItem>
-                <Link to="/login">Login</Link>
-                </NavItem>
-            </Nav>
-            </Collapse>
-        </Navbar>
-        </div>
-    );
+        return (
+            <div>
+                <Navbar className="topnav" dark expand="md">
+                    <NavbarBrand href="/"><img src={ require('../assets/images/utar.jpg') } /></NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <Link to="/">Home</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/society">Societies</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/event">Events</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/recruitmentBooth">Booths</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/myProfile">My Profile</Link>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Manage
+                            </DropdownToggle>
+                                <DropdownMenu left>
+                                    <DropdownItem name="createProfile" onClick={this.onClick}>
+                                        Create Society Profile
+                                    </DropdownItem>
+                                    <DropdownItem name="submitProposal" onClick={this.onClick}>
+                                        Submit Proposal
+                                    </DropdownItem>
+                                    <DropdownItem name="manageBooth" onClick={this.onClick}>
+                                        Manage Booth
+                                    </DropdownItem>
+                                    <DropdownItem name="manageProposal" onClick={this.onClick}>
+                                        Manage Proposal
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem name="faq" onClick={this.onClick}>
+                                        FAQ
+                                    </DropdownItem>
+                                </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Link to="/login">Login</Link>
+                        </NavItem>
+                    </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
     }
 }
 
