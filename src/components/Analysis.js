4576d21bd4ad899829d98e3883b385d67051ba0c
wanching_97 <wanching_97@hotmail.com>
2018-08-10 12:54:30 +0800

    added line graph

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import color from 'rcolor';
 import Graph from './Graph';
+import { Line } from '../../node_modules/react-chartjs-2';
 
 class Analysis extends React.Component {
 
@@ -8,11 +9,38 @@ class Analysis extends React.Component {
     
     render() {
 
-        
+        const data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+              {
+                label: 'My First dataset',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40]
+              }
+            ]
+          };
+
+          
         return (
             <div>
-            <h2>You can even make crazy graphs like this!</h2>
-             <Graph />
+            <h2>Line Example</h2>
+            <Line data={data} />
           </div>
         );
     }
