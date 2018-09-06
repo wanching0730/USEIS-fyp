7b5f8ed3adac5299b995590166311ca1b287fe5a
wanching_97 <wanching_97@hotmail.com>
2018-09-06 17:45:20 +0800

    done cardview

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import { Link } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Card, CardImg, CardText, CardBody,
+import { Breadcrumb, BreadcrumbItem, Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button } from 'reactstrap';
 import '../style/newsfeed.css';
 import {browserHistory} from 'react-router';
@@ -13,56 +12,7 @@ class NewsFeed extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            posts: {}
-        }
     }
-
-    componentWillMount() {
-        const PostsData = [
-            {
-              "category": "News",
-              "title": "CNN Acquire BEME",
-              "text": "CNN purchased Casey Neistat's Beme app for $25million.",
-              "image": "https://source.unsplash.com/user/erondu/600x400"
-            },
-            {
-              "category": "Travel",
-              "title": "Nomad Lifestyle",
-              "text": "Learn our tips and tricks on living a nomadic lifestyle",
-              "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
-            },
-            {
-              "category": "Development",
-              "title": "React and the WP-API",
-              "text": "The first ever decoupled starter theme for React & the WP-API",
-              "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
-            },
-            {
-              "category": "News",
-              "title": "CNN Acquire BEME",
-              "text": "CNN purchased Casey Neistat's Beme app for $25million.",
-              "image": "https://source.unsplash.com/user/erondu/600x400"
-            },
-            {
-              "category": "Travel",
-              "title": "Nomad Lifestyle",
-              "text": "Learn our tips and tricks on living a nomadic lifestyle",
-              "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
-            },
-            {
-              "category": "Development",
-              "title": "React and the WP-API",
-              "text": "The first ever decoupled starter theme for React & the WP-API",
-              "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
-            }
-          ]
-          
-        this.setState({
-          posts: PostsData
-        });
-    }    
 
     componentDidMount() {
         window.scrollTo(0, 0);
@@ -86,7 +36,7 @@ class NewsFeed extends Component {
 
                     <div className="card">
                         <Card>
-                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                            <CardImg top width="40px" height="300px" src={ require('../assets/images/its.jpg') } alt="Card image cap" />
                             <CardBody>
                             <CardTitle>Card title</CardTitle>
                             <CardSubtitle>Card subtitle</CardSubtitle>
