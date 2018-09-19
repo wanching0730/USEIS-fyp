13eb2c0537dda6e296bfe4cfc06e4294c13cf999
wanching_97 <wanching_97@hotmail.com>
2018-09-19 23:33:21 +0800

    added society data payload

--- src/actions/data-action.js
+++ src/actions/data-action.js
@@ -6,7 +6,21 @@ import {
     RETRIEVE_USER_EVENT
 } from '../constant';
 
-export function retrieveDataSuccessful() {
+export function retrieveSocietySuccessful(societyName, societyCategory, societyVision, societyMission, societyDesc) {
+    return {
+        type: RETRIEVE_USER_SOCIETY,
+        payload: {
+            societyName: societyName,
+            societyCategory: societyCategory,
+            societyVision: societyVision,
+            societyMission: societyMission,
+            societyDesc: societyDesc
+        }
+    }
+
+}
+
+export function retrieveEventSuccessful() {
 
 }
 
@@ -14,6 +28,18 @@ export function retrieveData(type, id) {
     return function (dispatch) {
         return getData(type, id).then(result => result.json()).then(reply => {
             console.log("result of get society: " + JSON.stringify(reply));
+
+            if(type == "society") {
+                let societyName = reply["name"];
+                let societyCategory = reply["category"];
+                let societyVision = reply["vision"];
+                let societyMission = reply["mission"];
+                let societyDesc = reply["description"];
+
+                dispatch(retrieveSocietySuccessful(societyName, societyCategory, societyVision, societyMission, societyDesc));
+            } else {
+                
+            }
         });
     };
 }
