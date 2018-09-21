4a19283b3f18659fbc61684a54d3f0c34c1b3596
wanching_97 <wanching_97@hotmail.com>
2018-09-22 01:07:40 +0800

    done Society page

--- src/components/Society.js
+++ src/components/Society.js
@@ -16,13 +16,10 @@ class Society extends Component {
     constructor(props) {
         super(props);
 
-        //this.state = {society: []};
-
         this.props.onRetrieveAll("society");
     }
 
     async componentDidMount() {
-        //this.listSocieties();
         window.scrollTo(0,0);
     }
 
@@ -30,10 +27,6 @@ class Society extends Component {
         const result = await fetch(`http://localhost:5000/puppies`);
         const jsonresult = await result.json();
         await this.setState({society: jsonresult});
-        // $.ajax({ type: "GET", url:`http://localhost:5000/puppies`, success: function(res) {
-        //     this.setState({society:res});
-        //  }});
-        //axios.get("http://localhost:5000/puppies").then(response => this.setState({society: response.data}));
     }
 
     groupBy(list, keyGetter) {
@@ -55,65 +48,37 @@ class Society extends Component {
     }
 
     render() {
-        // var list = [];
-        // for(var i = 0; i < this.state.society.length; i++) {
-        //     list.push(<p>{this.state.society[i]}</p>);
-        // }
-
         let societies = this.props.societies;
-        console.log("stringify societies: " + JSON.stringify(societies));
-        console.log("societies in Society: " + societies);
-
-
 
         if(societies != null) {
             var rows = [];
+            var counter = 1;
             const categories = this.groupBy(societies, society => society["category"]);
             for (const [key, values] of categories.entries()) {
                 var subRows = [];
                 values.forEach(value => {
                     subRows.push(
-                        <li><Link to={`/perEvent/` + value["id"]}>{value["name"]}</Link></li>
+                        <li><Link to={`/perSociety/` + value["id"]}>{value["name"]}</Link></li>
                     );
                 });
 
+                let stringId = "list-item-" + counter;
                 rows.push(
                     <li>
-                        <input type="checkbox" id="list-item-1" />
-                        <label for="list-item-1" className="first">{key}   <FontAwesome.FaHandORight /></label>
+                        <input type="checkbox" id={stringId} />
+                        <label for={stringId} className="first">{key}   <FontAwesome.FaHandORight /></label>
                         <ul>
                             {subRows}
                         </ul>
                     </li>
                 );
-              }
-            
-            
-            // var rows = [];
-            // for(var i = 0; i < societies.length; i++) {
-            //     let category = societies[i]["category"];
-            //     for(var j = 0; j < societies.length; j++) {
-            //         var subRows = [];
-            //         if(societies[j]["category"] == category) {
-            //             subRows.push(
-            //                 <li><Link to={`/perEvent/` + societies[j]["id"]}>{societies[j]["name"]}</Link></li>
-            //             );
-            //         }
-            //     }  
 
-            //     rows.push(
-            //         <li>
-            //             <input type="checkbox" id="list-item-1" />
-            //             <label for="list-item-1" className="first">{category}   <FontAwesome.FaHandORight /></label>
-            //             <ul>
-            //                {subRows}
-            //             </ul>
-            //         </li>
-            //     );
-            // }
+                <hr/>
+                counter++;
+              }
         }
 
-        const { imageStyle, RaisedButtonStyle } = styles;
+        const { RaisedButtonStyle } = styles;
         
         return (
             <div id="outerDiv"> 
@@ -126,8 +91,6 @@ class Society extends Component {
                     </Breadcrumb>
                 </div>
 
-                {rows}
-
                 <div>
                     <MuiThemeProvider>
                         <h1 style={{ margin: 20, color: '#083477' }}>Society List</h1>
@@ -137,177 +100,11 @@ class Society extends Component {
 
                         <div className="wrapper">
                             <ul>
-                                <li>
-                                    <input type="checkbox" id="list-item-1" />
-                                    <label for="list-item-1" className="first">Dance   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li><Link to={`/perEvent/1`}>WorkShop</Link></li>
-                                        <li><Link to={`/perEvent/1`}>KLESF</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Easy Parcel Talk</Link></li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-2"/>
-                                    <label for="list-item-2">Entertainment   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li><Link to={`/perEvent/1`}>Cardio Night Run</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Blood Donation</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Adventure Camp</Link></li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-3"/>
-                                    <label for="list-item-3">Engineering   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li><Link to={`/perEvent/1`}>ES Camp</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Engineering Fiesta</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Annual General Meeting</Link></li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-4" />
-                                    <label for="list-item-4">Information Technology   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li><Link to={`/perEvent/1`}>ES Camp</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Engineering Fiesta</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Annual General Meeting</Link></li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-5"/>
-                                    <label for="list-item-5">Music   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li><Link to={`/perEvent/1`}>Night Run</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Colour Run</Link></li>
-                                        <li><Link to={`/perEvent/1`}>Sport Carnival</Link></li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-6"/>
-                                    <label for="list-item-6">Political   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li>Inconsolata</li>
-                                        <li>Source Code Pro</li>
-                                        <li>Droid Sans Mono</li>
-                                        <li>Ubuntu Mono</li>
-                                        <li>Cousine</li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-7" />
-                                    <label for="list-item-7">Special Interest   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li>Slabo</li>
-                                        <li>Droid Serif</li>
-                                        <li>Roboto Serif</li>
-                                        <li>Lora</li>
-                                        <li>Meriweather</li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-8"/>
-                                    <label for="list-item-8">Spiritual   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li>Open Sans</li>
-                                        <li>Roboto</li>
-                                        <li>Lato</li>
-                                        <li>Stabo</li>
-                                        <li>Oswald</li>
-                                    </ul>
-                                </li>
-                                <hr/>
-                                <li>
-                                    <input type="checkbox" id="list-item-9"/>
-                                    <label for="list-item-9" className="last">Sport   <FontAwesome.FaHandORight /></label>
-                                    <ul>
-                                        <li>Inconsolata</li>
-                                        <li>Source Code Pro</li>
-                                        <li>Droid Sans Mono</li>
-                                        <li>Ubuntu Mono</li>
-                                        <li>Cousine</li>
-                                    </ul>
-                                </li>
+                                {rows}
                             </ul>
                         </div>
                         </MuiThemeProvider>
                     </div>
-
-                {/* <div className="container" id="tableContainer">
-                    <div className="row">
-                        <div className="panel-body">
-                            <table className="table table-hover table-light" border="1">
-                                <thead>
-                                    <tr>
-                                        <th>No.</th>
-                                        <th>Society Logos</th>
-                                        <th>Societies</th>  
-                                        <th>Events</th>               
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
-                                        <td><Link to={`/perSociety/1`}>IT Society</Link></td>
-                                        <td> 
-                                            <li><Link to={`/perEvent/1`}>WorkShop</Link></li>
-                                            <li><Link to={`/perEvent/1`}>KLESF</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Easy Parcel Talk</Link></li>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
-                                        <td><Link to={`/perSociety/1`}>First Aid Society</Link></td>
-                                        <td> 
-                                            <li><Link to={`/perEvent/1`}>Cardio Night Run</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Blood Donation</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Adventure Camp</Link></li>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
-                                        <td><Link to={`/perSociety/1`}>Engineering Society</Link></td>
-                                        <td> 
-                                            <li><Link to={`/perEvent/1`}>ES Camp</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Engineering Fiesta</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Annual General Meeting</Link></li>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td><img style={imageStyle} src={ require('../assets/images/image1.jpg') } /></td>
-                                        <td><Link to={`/perSociety/1`}>Sport Society</Link></td>
-                                        <td> 
-                                            <li><Link to={`/perEvent/1`}>Night Run</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Colour Run</Link></li>
-                                            <li><Link to={`/perEvent/1`}>Sport Carnival</Link></li>
-                                        </td>
-                                    </tr>
-                                   
-                                    {this.state.society.map(row => {
-                                        return (
-                                            <tr>
-                                                <td><Link to={`/perSociety/`+row[0]}>{row[0]}</Link></td>
-                                                <td>{row[1]}</td>
-                                                <td>{row[3]}</td>
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div> */}
             </div>
         );
     };
@@ -315,10 +112,6 @@ class Society extends Component {
 };
 
 const styles = {
-    imageStyle: {
-        height: "50px",
-        width: "50px"
-    },
     RaisedButtonStyle: {
         marginLeft: 20
     }
