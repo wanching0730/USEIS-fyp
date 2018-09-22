6b6289dd8432b2b1bad3c95e8d60dca65e34118f
wanching_97 <wanching_97@hotmail.com>
2018-09-22 12:07:47 +0800

    edited post-action

--- src/actions/post-action.js
+++ src/actions/post-action.js
@@ -6,6 +6,24 @@ import {
     CREATE_EVENT
 } from '../constant';
 
+export function createSocietySuccessful(societyId) {
+    return {
+        type: CREATE_SOCIETY,
+        payload: {
+            createdSocietyId: societyId
+        }
+    }
+}
+
+export function createEventSuccessful(eventId) {
+    return {
+        type: CREATE_EVENT,
+        payload: {
+            createdEventId: eventId
+        }
+    }
+}
+
 export function create(type, postData) {
     return function (dispatch) {
         const data = Object.keys(postData).map((key) => {
@@ -14,6 +32,14 @@ export function create(type, postData) {
 
         return createData(data).then(result => result.json()).then(reply => {
             console.log("post data reply: " + reply);
+
+            // if(type == "society") {
+            //     dispatch(createSocietySuccessful(id));
+            //     browserHistory.push("/perSociety/" + id);
+            // } else {
+            //     dispatch(createEventSuccessful(id));
+            //     browserHistory.push("/societyEvents");
+            // }
         });
     };
 }
