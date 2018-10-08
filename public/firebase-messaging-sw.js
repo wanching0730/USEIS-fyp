1a4c85f2d2502b745d419f38ccab60447fd9cd81
wanching_97 <wanching_97@hotmail.com>
2018-10-08 23:13:50 +0800

    fcm token success and solved cors

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,5 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.3/firebase.js');
 
 firebase.initializeApp({
     messagingSenderId: "938674404737"
--- src/push-notification.js
+++ src/push-notification.js
@@ -2,14 +2,38 @@ import firebase from 'firebase';
 
 export const initializeFirebase = () => {
   firebase.initializeApp({
+    apiKey: "AIzaSyBkLaohp-Gm56OUnPetImLOTg46gV6LSQA",
+    authDomain: "pwa-app-36fe6.firebaseapp.com",
+    databaseURL: "https://pwa-app-36fe6.firebaseio.com",
+    projectId: "pwa-app-36fe6",
+    storageBucket: "pwa-app-36fe6.appspot.com",
     messagingSenderId: "938674404737"
   });
 
-  navigator.serviceWorker
-    .register('/registerServiceWorker.js')
-    .then((registration) => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
       firebase.messaging().useServiceWorker(registration);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
     });
+  }
+
+  navigator.serviceWorker.ready
+    .then(function(serviceWorkerRegistration) {
+        return serviceWorkerRegistration.pushManager.subscribe({
+            userVisibleOnly: true
+        });
+    })
+    .then(function(subscription) {console.log(subscription.endpoint);});
+
+//   navigator.serviceWorker
+//     .register('../firebase-messaging-sw.js', {
+//         scope: '/'
+//     }).then((registration) => {
+//       firebase.messaging().useServiceWorker(registration);
+//     });
 }
 
 export const askForPermissioToReceiveNotifications = async () => {
@@ -26,24 +50,25 @@ export const askForPermissioToReceiveNotifications = async () => {
 }
 
 export function initializePush() {
-const messaging = firebase.messaging();
-messaging
-    .requestPermission()
-    .then(() => {
-        console.log("Have Permission");
-        return messaging.getToken();
-    })
-    .then(token => {
-        console.log("FCM Token:", token);
-        //you probably want to send your new found FCM token to the
-        //application server so that they can send any push
-        //notification to you.
-    })
-    .catch(error => {
-        if (error.code === "messaging/permission-blocked") {
-            console.log("Please Unblock Notification Request Manually");
-        } else {
-            console.log("Error Occurred", error);
-        }
-        });
+    const messaging = firebase.messaging();
+    messaging.usePublicVapidKey("BKCWz7kE-vlcFudrN0S4M9z-RTZVp8J-ncVbYQoRgObAeDfJEO8bHNYL0dgtTlpxRclWNUci_YwvfYUtbUK9lqQ");
+    messaging
+        .requestPermission()
+        .then(() => {
+            console.log("Have Permission");
+            return messaging.getToken();
+        })
+        .then(token => {
+            console.log("FCM Token:", token);
+            //you probably want to send your new found FCM token to the
+            //application server so that they can send any push
+            //notification to you.
+        })
+        .catch(error => {
+            if (error.code === "messaging/permission-blocked") {
+                console.log("Please Unblock Notification Request Manually");
+            } else {
+                console.log("Error Occurred", error);
+            }
+            });
 }
