dcb960a4a541a4605ad99a1104639b5baec74d12
wanching_97 <wanching_97@hotmail.com>
2019-01-17 01:16:37 +0800

    removed unwanted code

--- src/utils/http_function.js
+++ src/utils/http_function.js
@@ -12,70 +12,18 @@ export function verifyUser(data) {
 
 export function getData(type, id) {
     return fetch(mainApiRoute + `/` + type + `/` + id);
-
-    // if(type === "society") {
-    //     return fetch(mainApiRoute + `/society/` + id);
-    // } else if(type === "event") {
-    //     return fetch(mainApiRoute + `/event/` + id);
-    // } else if(type === "societyEvent") {
-    //     return fetch(mainApiRoute + `/societyEvent/` + id);
-    // } else if(type === "studentEvent") {
-    //     return fetch(mainApiRoute + `/studentEvent/` + id);
-    // } else if(type === "staffEvent") {
-    //     return fetch(mainApiRoute + `/staffEvent/` + id);
-    // } else if(type === "eventCrew") {
-    //     return fetch(mainApiRoute + `/eventCrew/` + id);
-    // } else if(type === "societyComm") {
-    //     return fetch(mainApiRoute + `/societyComm/` + id);
-    // } else if(type === "eventComm") {
-    //     return fetch(mainApiRoute + `/eventComm/` + id);
-    // } else if(type === "societyMember") {
-    //     return fetch(mainApiRoute + `/societyMember/` + id);
-    // } else if(type === "eventParticipant") {
-    //     return fetch(mainApiRoute + `/eventParticipant/` + id);
-    // }
 }
 
 export function getDataWithUserId(type, id, userId) {
     return fetch(mainApiRoute + `/` + type + `/` + id + `/` + userId);
-
-    // if(type === "studentSociety") {
-    //     return fetch(mainApiRoute + `/studentSociety/` + id + `/` + userId);
-    // } else if(type === "staffSociety") {
-    //     return fetch(mainApiRoute + `/staffSociety/` + id + `/` + userId);
-    // } else if(type === "studentEvent") {
-    //     return fetch(mainApiRoute + `/studentEvent/` + id + `/` + userId);
-    // } else {
-    //     return fetch(mainApiRoute + `/staffEvent/` + id + `/` + userId);
-    // }
 }
 
 export function getAllData(type) {
     return fetch(mainApiRoute + `/` + type);
-
-    // if(type === "society") {
-    //     return fetch(mainApiRoute + `/societies`);
-    // } else if(type === "event") {
-    //     return fetch(mainApiRoute + `/events`);
-    // } else if(type === "newsfeeds") {
-    //     return fetch(mainApiRoute + `/newsfeeds`);
-    // } else if(type === "allSocietyEvent") {
-    //     return fetch(mainApiRoute + `/societyEvent`);
-    // } else if(type === "societyBooth") {
-    //     return fetch(mainApiRoute + `/societyBooth`);
-    // } else if(type === "eventBooth") {
-    //     return fetch(mainApiRoute + `/eventBooth`);
-    // } 
 }
 
 export function searchAllData(type, keyword) {
     return fetch(mainApiRoute + `/search/` + type + `/` + keyword);
-
-    // if(type === "society") {
-    //     return fetch(mainApiRoute + `/search/society/` + keyword);
-    // } else if(type === "event") {
-    //     return fetch(mainApiRoute + `/search/event/` + keyword);
-    // } 
 }
 
 export function createData(type, data) {
@@ -156,26 +104,4 @@ export function deleteData(type, id, eventId) {
     return fetch(mainApiRoute + `/` + type + `/` + id + `/` + eventId, {
         method: 'DELETE'
     });
-
-    // if(type === "studentEvent") {
-    //     return fetch(mainApiRoute + `/studentEvent/` + id + '/' + eventId, {
-    //         method: 'DELETE'
-    //     });
-    // } else if(type === "staffEvent") {
-    //     return fetch(mainApiRoute + `/staffEvent/` + id + '/' + eventId, {
-    //         method: 'DELETE'
-    //     });
-    // } else if(type === "eventCrew") {
-    //     return fetch(mainApiRoute + `/eventCrew/` + id + '/' + eventId, {
-    //         method: 'DELETE'
-    //     });
-    // } else if(type === "studentParticipant") {
-    //     return fetch(mainApiRoute + `/studentParticipant/` + id + '/' + eventId, {
-    //         method: 'DELETE'
-    //     });
-    // } else if(type === "staffParticipant") {
-    //     return fetch(mainApiRoute + `/staffParticipant/` + id + '/' + eventId, {
-    //         method: 'DELETE'
-    //     });
-    // } 
 }
